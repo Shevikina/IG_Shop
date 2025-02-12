c4bc6a3c464d240eab39c78c6a0713227c3a0e91
Shevikina <Shevik.vlog@gmail.com>
2025-02-12 11:42:49 +0300

    Add SpecialOffersWidget

---
+++ app/src/main/java/com/ig81/igshop/ui/screens/home/views/SpecialOffersWidget.kt
@@ -0,0 +1,85 @@
+package com.ig81.igshop.ui.screens.home.views
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ig81.igshop.ui.theme.IGShopTheme
+
+data class CardInfo(val num: String, val alpha: Float, var selected: Boolean = false)
+
+@Composable
+fun SpecialOffersWidget() {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            text = "Спецпредложения",
+            style = IGShopTheme.typography.bodyLarge.copy(
+                fontWeight = FontWeight.Bold,
+                fontSize = 18.sp,
+                lineHeight = 21.09.sp,
+                color = IGShopTheme.colorScheme.onBackground
+            )
+        )
+
+        val cards = listOf(
+            CardInfo("№ 364 154", 0.03f),
+            CardInfo("№ 121 453", 0.08f),
+            CardInfo("№ 105 423", 0.18f),
+            CardInfo("№ 103 436", 0.23f, selected = true)
+        )
+        LazyRow(
+            horizontalArrangement = Arrangement.spacedBy((-166).dp),
+            reverseLayout = true,
+        ) {
+            items(cards) { card ->
+                Box(
+                    modifier = Modifier
+                        .background(
+                            IGShopTheme.colorScheme.background,
+                            IGShopTheme.shapes.dialog
+                        )
+                        .wrapContentSize()
+                ) {
+
+                    SpecialOfferCard(
+                        card.num,
+                        card.selected,
+                        card.alpha,
+                        modifier = Modifier
+                            .size(206.dp, 125.dp)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun SpecialOffersWidgetPreview() {
+    IGShopTheme {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(IGShopTheme.colorScheme.background)
+                .padding(32.dp)
+        ) {
+            SpecialOffersWidget()
+        }
+    }
+}
