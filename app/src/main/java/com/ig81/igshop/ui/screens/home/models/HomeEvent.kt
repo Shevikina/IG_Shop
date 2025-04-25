794864d9e94ece5120a4470b1088d45c7bd851ca
Shevikina <Shevik.vlog@gmail.com>
2025-04-25 18:06:25 +0300

    Upd HomeEvent

--- app/src/main/java/com/ig81/igshop/ui/screens/home/models/HomeEvent.kt
+++ app/src/main/java/com/ig81/igshop/ui/screens/home/models/HomeEvent.kt
@@ -3,4 +3,8 @@ package com.ig81.igshop.ui.screens.home.models
 sealed class HomeEvent {
     data object EnterScreen : HomeEvent()
     data object ReloadScreen : HomeEvent()
+    data object OpenSearchScreen : HomeEvent()
+    data object OpenSportsCarPageScreen : HomeEvent()
+    data object OpenAchievementsScreen : HomeEvent()
+
 }
