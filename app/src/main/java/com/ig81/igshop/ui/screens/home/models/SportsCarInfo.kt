c70a32cb71744210e2b3de1cf7a7ca5d00d8ddc0
Shevikina <Shevik.vlog@gmail.com>
2025-04-25 19:04:04 +0300

    Upd SportsCarInfo

--- app/src/main/java/com/ig81/igshop/ui/screens/home/models/SportsCarInfo.kt
+++ app/src/main/java/com/ig81/igshop/ui/screens/home/models/SportsCarInfo.kt
@@ -1,10 +1,14 @@
 package com.ig81.igshop.ui.screens.home.models
 
 data class SportsCarInfo(
+    val id: Int,
     val carName: String,
     val carType: String,
-    var imagePath: String,
-    var rating: Int,
-    var price: Int,
-    val onClick: (() -> Unit)? = null
+    val imagePath: String,
+    val rating: Int,
+    val price: Int,
+    val speedRating: Int,
+    val caseRating: Int,
+    val shieldsRating: Int,
+    val description: String = ""
 )
