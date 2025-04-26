2cb2ea9d05742d41f72b0d564263393ac3becc28
Shevikina <Shevik.vlog@gmail.com>
2025-04-26 15:53:31 +0300

    Upd AchievementsEvent

--- app/src/main/java/com/ig81/igshop/ui/screens/achievements/models/AchievementsEvent.kt
+++ app/src/main/java/com/ig81/igshop/ui/screens/achievements/models/AchievementsEvent.kt
@@ -2,6 +2,8 @@ package com.ig81.igshop.ui.screens.achievements.models
 
 sealed class AchievementsEvent {
     data object EnterScreen : AchievementsEvent()
+    data object CloseScreen : AchievementsEvent()
+    data object ViewFullList : AchievementsEvent()
     data object ReloadScreen : AchievementsEvent()
     data class ChangeQuery(val newValue: String) : AchievementsEvent()
     data class ChangeTab(val num: Int) : AchievementsEvent()
