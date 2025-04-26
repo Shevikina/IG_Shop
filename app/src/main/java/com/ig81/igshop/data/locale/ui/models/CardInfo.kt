dab0aea19ad671e86b2e56a4c885de40c473a25b
Shevikina <Shevik.vlog@gmail.com>
2025-04-26 13:06:20 +0300

    Remove CardInfo

--- app/src/main/java/com/ig81/igshop/data/locale/ui/models/CardInfo.kt
+++
@@ -1,8 +0,0 @@
-package com.ig81.igshop.data.locale.ui.models
-
-data class CardInfo(
-    val num: String,
-    val alpha: Float,
-    var selected: Boolean = false,
-    val onClick: (() -> Unit)? = null
-)
