463b33e19a9dad2f0cb8554d7df0708d1882a487
Shevikina <Shevik.vlog@gmail.com>
2025-04-26 12:44:41 +0300

    Upd CarPageEvent

--- app/src/main/java/com/ig81/igshop/ui/screens/car/page/models/CarPageEvent.kt
+++ app/src/main/java/com/ig81/igshop/ui/screens/car/page/models/CarPageEvent.kt
@@ -3,5 +3,6 @@ package com.ig81.igshop.ui.screens.car.page.models
 sealed class CarPageEvent {
     data class EnterScreen(val carId: Int) : CarPageEvent()
     data object ReloadScreen : CarPageEvent()
+    data object CloseScreen : CarPageEvent()
     data object BookCar : CarPageEvent()
 }
