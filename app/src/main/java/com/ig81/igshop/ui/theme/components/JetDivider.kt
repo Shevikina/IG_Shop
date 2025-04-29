39cfc617e4dda607502add174d73a8e58473cdae
Shevikina <Shevik.vlog@gmail.com>
2025-04-29 19:28:55 +0300

    Upd JetDivider

--- app/src/main/java/com/ig81/igshop/ui/theme/components/JetDivider.kt
+++ app/src/main/java/com/ig81/igshop/ui/theme/components/JetDivider.kt
@@ -2,14 +2,14 @@ package com.ig81.igshop.ui.theme.components
 
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.ig81.igshop.ui.theme.IGShopTheme
 
 @Composable
-fun JetDivider(paddingValues: PaddingValues = PaddingValues(vertical = 8.dp)) = Divider(
+fun JetDivider(paddingValues: PaddingValues = PaddingValues(vertical = 8.dp)) = HorizontalDivider(
     modifier = Modifier.padding(paddingValues),
     thickness = 2.dp,
     color = IGShopTheme.colorScheme.tertiary.copy(0.25f)
